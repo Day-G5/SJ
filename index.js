a78b379975d9419432f5abc4ff0e980f86cc8d26
Day_G <dejikayode5@gmail.com>
2024-11-10 17:44:06 +0100

    Add z-index animation and slider rotation functionality

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const body = document.getElementsByTagName('body')[0];
 
+const banner = document.getElementsByClassName('banner')[0];
+
 const container = document.getElementsByClassName('slider')[0];
 
 const arr = [];
 
-let top = 0;
+const quantity_before_new_loop = 10;
+
+let top = 10;
+
+let z_index = 27;
+
+function updateZIndex(slider, index_z) {
+    const computedStyle = window.getComputedStyle(slider);
+    const matrix = new DOMMatrix(computedStyle.transform);
+    const angle = Math.atan2(matrix.m32, matrix.m33) * (180 / Math.PI);
+
+    if(angle >= 70 && angle <= 250) {
+        // Calculate the swapped z-index
+        const swappedIndex = 29 - index_z; // This formula will swap: 2↔27, 3↔26, 4↔25, etc.
+        slider.style.zIndex = `${swappedIndex}`;
+    } else {
+        slider.style.zIndex = `${index_z}`;
+    }
+   
+    requestAnimationFrame(() => updateZIndex(slider, index_z));
+}
 
 class _3d_tile {
     constructor(text, image, number_of_items, index) {
@@ -16,8 +38,60 @@ class _3d_tile {
 
         this.tile.style.setProperty('--position', index);
 
+        this.tile.style.transform = `rotateY(calc( (${index} - 1) * (360 / ${number_of_items}) * 1deg))
+        translateZ(550px) translateY(120px)`;
+
         this.tile.appendChild(this.img);
         container.appendChild(this.tile);
+    }
+}
+
+class banner_step {
+    constructor (text, image, number_of_items, index) {
+        // this.banner = document.createElement('div');
+        // this.banner.classList.add("banner");
+
+        this.slider = document.createElement("div");
+        this.slider.classList.add("slider");
+
+        this.slider.style.setProperty('--quantity', 1);
+        this.slider.style.top = top + '%';
+
+        top += 10;
+
+        if(z_index <= 3){
+            z_index = 27
+        };
+
+        this.zIndex = z_index;
+        this.slider.style.zIndex = `${this.zIndex}`;
+
+        z_index--;
+
+        this.item = document.createElement("div");
+        this.item.classList.add("item");
+
+        this.item.style.setProperty('--position', index);
+
+        this.position = index;
+        
+        if(index > quantity_before_new_loop){
+            this.position = (Math.floor(index / quantity_before_new_loop) + (index % quantity_before_new_loop)) - quantity_before_new_loop - 1;
+        }
+
+        this.item.style.transform = `rotateY(calc( (${this.position} - 1) * (360 / ${quantity_before_new_loop}) * 1deg))
+        translateZ(550px) translateY(120px)`;
+
+        this.image = document.createElement("img");
+        this.image.classList.add("image");
+
+        this.image.src = image;
+
+        this.item.appendChild(this.image);
+        this.slider.appendChild(this.item);
+        banner.appendChild(this.slider);
+
+        requestAnimationFrame(() => updateZIndex(this.slider, this.zIndex));
     }
 }
 
@@ -29,7 +103,7 @@ const myVariable = await fetch('./data.json')
     container.style.setProperty('--number-of-items', number_of_items);
 
     Object.values(data).forEach((element, index) => {
-        arr.push(new _3d_tile(element.text, element.img, number_of_items, index+1));
+        arr.push(new banner_step(element.text, element.img, number_of_items, index+1));
     });
 })
 .finally(console.log('done'));
